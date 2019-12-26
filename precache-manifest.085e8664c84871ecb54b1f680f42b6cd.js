2cd505311a17204d59f04008c17d2e90772bbbbf
art1shok <kvitkaart554@gmail.com>
2019-12-26 10:00:18 +0200

    Updates

---
+++ precache-manifest.085e8664c84871ecb54b1f680f42b6cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13be5bbccfe6ff116d27c3d908509d32",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "165a17d0cc1d46e7716d",
+    "url": "/react_dynamic-list-of-posts/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "4cb32e50a5593bd5cadd",
+    "url": "/react_dynamic-list-of-posts/static/js/2.ec804f90.chunk.js"
+  },
+  {
+    "revision": "165a17d0cc1d46e7716d",
+    "url": "/react_dynamic-list-of-posts/static/js/main.6556b091.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
